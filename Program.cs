6f6699d65bc20d4467458eaa86cca90221f92726
Adam Lapčák <xlapca00@vutbr.cz>
2021-03-04 14:59:16 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string testovaciText = "Toto je retezec predstavovany nekolika radky,\n"
+            + "ktere jsou od sebe oddeleny znakem LF (Line Feed).\n"
+            + "Je tu i nejaky ten vykricnik! Pro ucely testovani i otaznik?\n"
+            + "Toto je jen zkratka zkr. covid ale ne konec vety. A toto je\n"
+            + "posledni veta!";
+
+            StringStatistics test = new StringStatistics(testovaciText);
+
+            Console.WriteLine("Number of words: {0}\n", test.Wordn());
+
+            Console.WriteLine("Number of rows: {0}\n", test.Rown());
+
+            Console.WriteLine("Number of senteces: {0}\n", test.Sentencen());
+
+            Console.WriteLine("Longest word(s): ");
+            Console.WriteLine(test.Longest());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Shortest word(s): ");
+            Console.WriteLine(test.Shortest());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Most used word(s): ");
+            Console.WriteLine(test.Most());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Words in alphabetical order: ");
+            Console.WriteLine(test.Sortw());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Is string infected with covid-19? ");
+            Console.WriteLine(test.IsInfected());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringStatistics.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class StringStatistics
+    {
+        public string str;
+        public char[] symbolw = new char[] { ' ', '.', ',', ';', '!', '(', ')', '?', '\n' };
+        public char[] symboln = new char[] { '\n' };
+        public char[] symbols = new char[] { '.', '!', '?' };
+        public string[] WordArray;
+        public string[] RowArray;
+        public string[] SentenceArray;
+
+        public StringStatistics(string temp)
+        {
+            str = temp;
+            WordArray = str.Split(symbolw, StringSplitOptions.RemoveEmptyEntries);
+            RowArray = str.Split(symboln);
+            SentenceArray = str.Split(symbols, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+       
+
+        public int Wordn()
+        {
+            return WordArray.Length;
+        }
+
+        public int Rown()
+        {
+            return RowArray.Length;
+        }
+
+        public int Sentencen()
+        {
+            int i = 0;
+            string UpperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            foreach (string sentence in SentenceArray)
+            {
+                if (UpperLetters.Contains(sentence[0]) || UpperLetters.Contains(sentence[1]))
+                {
+                    i++;
+                }
+            }
+
+            return i;
+        }
+
+        public string Longest()
+        {
+            int lenght = 0;
+            string final = "";
+
+            foreach (string word in WordArray)
+            {
+                if (lenght < word.Length)
+                {
+                    lenght = word.Length;
+                }
+            }
+
+            foreach (string word in WordArray)
+            {
+                if (lenght == word.Length)
+                {
+                    final = final + word + " ";
+                }
+            }
+
+            return final;
+
+        }
+
+        public string Shortest()
+        {
+            int lenght = Int32.MaxValue;
+            string final = "";
+
+            foreach (string word in WordArray)
+            {
+                if (lenght > word.Length)
+                {
+                    lenght = word.Length;
+                }
+            }
+
+            foreach (string word in WordArray)
+            {
+                if (lenght == word.Length)
+                {
+                    final = final + word + " ";
+                }
+            }
+
+            return final;
+        }
+
+        public string Sortw()
+        {
+            Array.Sort(WordArray);
+            string final = "";
+
+            foreach (string word in WordArray)
+            {
+                final = final + word + " ";
+            }
+
+            return final;
+        }
+
+        public string Most()
+        {
+            Array.Sort(WordArray);
+            int temp = 1;
+            int current = 1;
+            string final = "";
+
+            for (int i = 1; i < WordArray.Length-1; i++)
+            {
+                
+                if(WordArray[i-1]==WordArray[i])
+                {
+                    temp++;
+                    if(temp>current)
+                    {
+                        current = temp;
+                        final = WordArray[i];
+                    }
+                }
+                else
+                {
+                    temp = 1;
+                }
+            }
+
+            return final;
+        }
+
+        public bool IsInfected()
+        {
+            string covid1 = "covid";
+            string covid2 = "covid-19";
+            string covid3 = "sars-cov-2";
+
+            if (str.Contains(covid1) || str.Contains(covid2) || str.Contains(covid3))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
